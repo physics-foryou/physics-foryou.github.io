2536d8f31ea0a9e891e895ece25592d7ad4b69d3
physics-foryou <physicsforyou0@gmail.com>
2026-06-23 16:58:29 -0700

    Create ap1-practice-exam-1.js

---
+++ ap1/ap1-practice-exam-1.js
@@ -0,0 +1,14 @@
+const ap1PracticeExamOnePool = [
+    {
+        id: "",
+        unit: "",
+        topic: "",
+        skill: "",
+        difficulty: "",
+        image: "",
+        question: "",
+        choices: [],
+        correctIndex: 0,
+        explanation: ""
+    }
+];
