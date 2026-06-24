3aa186d0ee5a3123f1a1e8d1536be1f4d03315f3
physics-foryou <physicsforyou0@gmail.com>
2026-06-23 18:26:37 -0700

    Update apc-eandm-practice-exam-mcq-pool-3.js

--- apc-eandm/apc-eandm-practice-exam-mcqs/apc-eandm-practice-exam-mcq-pool-3.js
+++ apc-eandm/apc-eandm-practice-exam-mcqs/apc-eandm-practice-exam-mcq-pool-3.js
@@ -1,1 +1,14 @@
-
+const apcEandmPracticeExamPoolThree = [
+    {
+        id: "",
+        unit: "",
+        topic: "",
+        skill: "",
+        difficulty: "",
+        image: "",
+        question: "",
+        choices: [],
+        correctIndex: 0,
+        explanation: ""
+    }
+];
