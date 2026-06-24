2810c0154cd24c70c6c6f6d6c23f884da7ef872b
physics-foryou <physicsforyou0@gmail.com>
2026-06-23 18:22:12 -0700

    Update ap1-practice-exam-mcq-pool-3.js

--- ap1/ap1-practice-exam-mcqs/ap1-practice-exam-mcq-pool-3.js
+++ ap1/ap1-practice-exam-mcqs/ap1-practice-exam-mcq-pool-3.js
@@ -1,1 +1,14 @@
-
+const ap1PracticeExamPoolThree = [
+    {
+        id: "",
+        unit: "",
+        topic: "",
+        skill: "",
+        difficulty: "",
+        image: "",
+        question: "",
+        choices: [],
+        correctIndex: 0,
+        explanation: ""
+    }
+];
