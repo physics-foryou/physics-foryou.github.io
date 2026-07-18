ac3b988f02cf7e3f534b762ef2e7b00ddd916371
physics-foryou <physicsforyou0@gmail.com>
2026-07-18 01:54:08 -0700

    Self Grade FRQ and Final Screen

---
+++ ap1/ap1-practice-exam-frq-extras/ap1-practice-exam-1-extras.js
@@ -0,0 +1,11 @@
+window.practiceExamScoreRequirements = window.practiceExamScoreRequirements || {};
+
+window.practiceExamScoreRequirements['ap1-1'] = {
+	scoreBands: [
+		{ score: 5, minPoints: 35, maxPoints: 50 },
+		{ score: 4, minPoints: 27, maxPoints: 34 },
+		{ score: 3, minPoints: 20, maxPoints: 26 },
+		{ score: 2, minPoints: 11, maxPoints: 19 },
+		{ score: 1, minPoints: 0, maxPoints: 10 }
+	]
+};
